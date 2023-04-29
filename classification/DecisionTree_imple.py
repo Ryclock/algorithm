d04e34ec7de78b96eda50f46b21c3efef800a07c
ChildJ <rythme.clock@gmail.com>
2023-04-29 12:14:33 +0800

    update DecisitionTree for classfication

--- classification/DecisionTree_imple.py
+++ classification/DecisionTree_imple.py
@@ -1,94 +1,99 @@
+import numpy as np
+from collections import Counter
 import pandas as pd
-import math
 from pathlib import Path
 import os
 
 
-def entropy(data):
-    label_counts = {}
-    for row in data:
-        key = row[-1]
-        label_counts[key] = label_counts.get(key, 0)+1
-    entropy = 0.0
-    for key in label_counts:
-        prob = float(label_counts[key]) / len(data)
-        entropy -= prob * math.log(prob, 2)
-    return entropy
+class Node:
+    def __init__(self, feature=None, entropy=None, classification=None, sub_nodes=None):
+        self.feature = feature
+        self.entropy = entropy
+        self.classification = classification
+        self.sub_nodes = sub_nodes
+
+    def predict(self, x):
+        if self.sub_nodes is None:
+            return self.classification
+        else:
+            x_new = np.delete(x, self.feature)
+            return self.sub_nodes[x[self.feature]].predict(x_new)
 
 
-def info_gain(data, feature_idx):
-    feature_counts = {}
-    for row in data:
-        key = row[feature_idx]
-        feature_counts[key] = feature_counts.get(key, 0)+1
-    info_gain = entropy(data)
-    for key in feature_counts:
-        prob = float(feature_counts[key]) / len(data)
-        subset = [row for row in data if row[feature_idx] == key]
-        info_gain -= prob * entropy(subset)
-    return info_gain
+def call_entropy(D):
+    count = np.array(list(Counter(D[:, -1]).values()))
+    p = count / sum(count)
+    return -sum(p * np.log2(p))
 
 
-def choose_best_feature(data, features):
-    '''
-        features: only be used to print information
-    '''
-    best_feature_idx = -1
-    best_info_gain = 0.0
-    for feature_idx in range(len(data[0]) - 1):
-        info_gain_current = info_gain(data, feature_idx)
-        print('IG({}): {}'.format(
-            features[feature_idx], info_gain_current), file=output_file)
-        if info_gain_current <= best_info_gain:
-            continue
-        best_info_gain = info_gain_current
-        best_feature_idx = feature_idx
-    return best_feature_idx
+def call_info_gain(D, A):
+    entropy_D = call_entropy(D)
+    uniq_A = np.unique(A)
+    entropy_DA = 0
+    for a in uniq_A:
+        D_a = D[A == a]
+        entropy_Da = call_entropy(D_a)
+        entropy_DA += entropy_Da * (len(D_a) / len(D))
+    return entropy_D - entropy_DA
 
 
-def majority_label(label_list):
-    return max(label_list, key=label_list.count)
+def id3(D, A):
+    feature_idx = np.argmax([call_info_gain(D, A[:, i])
+                            for i in range(len(A[0]))])
+    uniq_vals = np.unique(A[:, feature_idx])
+    node = Node(feature_idx)
+    sub_nodes = {}
+    for val in uniq_vals:
+        D_val = D[A[:, feature_idx] == val]
+        if len(D_val) == 0:
+            sub_nodes[val] = Node(None, 0, Counter(
+                D[:, -1]).most_common(1)[0][0])
+        elif len(np.unique(D_val[:, -1])) == 1:
+            sub_nodes[val] = Node(None, 0, D_val[0, -1])
+        else:
+            sub_nodes[val] = id3(D_val, np.delete(
+                A[A[:, feature_idx] == val], feature_idx, axis=1))
+    node.sub_nodes = sub_nodes
+    return node
 
 
-def get_sub_data(data, feature_idx, value):
-    return [row[:feature_idx]+row[feature_idx+1:]
-            for row in data if row[feature_idx] == value]
+def call_gr(D, A):
+    entropy_D = call_entropy(D)
+    uniq_A = np.unique(A)
+    entropy_DA = 0
+    iv_A = 0
+    for a in uniq_A:
+        D_a = D[A == a]
+        entropy_Da = call_entropy(D_a)
+        entropy_DA += entropy_Da * (len(D_a) / len(D))
+        iv_A -= (len(D_a) / len(D)) * np.log2(len(D_a) / len(D))
+    if iv_A == 0:
+        return float('inf')
+    else:
+        return (entropy_D - entropy_DA) / iv_A
 
 
-def create_tree(data, features, floor):
-    '''
-        floor: only be used to print information
-    '''
-    print('Floor: {}'.format(floor), file=output_file)
-    label_list = [row[-1] for row in data]
-    if label_list.count(label_list[0]) == len(label_list):
-        print('unique label: {}'.format(label_list[0]), file=output_file)
-        return label_list[0]
-    if len(data[0]) == 1:
-        return majority_label(label_list)
-    best_feature_idx = choose_best_feature(data, features)
-    best_feature = features[best_feature_idx]
-    print('Chosen(best): {}'.format(best_feature), file=output_file)
-    tree = {best_feature: {}}
-    sub_features = features[:best_feature_idx]+features[best_feature_idx+1:]
-    for value in set([row[best_feature_idx] for row in data]):
-        print('\nNow analysize value: {}'.format(value), file=output_file)
-        sub_data = get_sub_data(data, best_feature_idx, value)
-        tree[best_feature][value] = create_tree(
-            sub_data, sub_features, floor+1)
-    return tree
-
-
-def predict(tree, features, sample):
-    root_label = list(tree.keys())[0]
-    root_node = tree[root_label]
-    feature_idx = features.index(root_label)
-    for key in root_node:
-        if sample[feature_idx] != key:
-            continue
-        if not isinstance(root_node[key], dict):
-            return root_node[key]
-        return predict(root_node[key], features, sample)
+def c45(D, A, delta=0):
+    feature_idx = np.argmax([call_gr(D, A[:, i]) for i in range(len(A[0]))])
+    info_gain_ratio = call_gr(D, A[:, feature_idx])
+    uniq_vals = np.unique(A[:, feature_idx])
+    node = Node(feature_idx)
+    sub_nodes = {}
+    for val in uniq_vals:
+        D_val = D[A[:, feature_idx] == val]
+        if len(D_val) == 0:
+            sub_nodes[val] = Node(None, 0, Counter(
+                D[:, -1]).most_common(1)[0][0])
+        elif len(np.unique(D_val[:, -1])) == 1:
+            sub_nodes[val] = Node(None, 0, D_val[0, -1])
+        elif info_gain_ratio < delta:
+            sub_nodes[val] = Node(None, 0, Counter(
+                D_val[:, -1]).most_common(1)[0][0])
+        else:
+            sub_nodes[val] = c45(D_val, np.delete(
+                A[A[:, feature_idx] == val], feature_idx, axis=1))
+    node.sub_nodes = sub_nodes
+    return node
 
 
 if __name__ == "__main__":
@@ -96,11 +101,11 @@ if __name__ == "__main__":
     paths = ['data_sets', 'watermelon']
     df = pd.read_csv(filepath_or_buffer=os.path.join(
         parent_path, *paths, 'data.csv'), index_col=0)
-    features = df.columns.values.tolist()
-    output_file = open(file="output.txt", mode='w+', encoding='utf-8')
-    print('DFS型输出\n', file=output_file)
-    tree = create_tree(df.values.tolist(), features, 1)
-    output_file.close()
-    # res = predict(tree, ['色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '好瓜'],
-    #               ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '硬滑'])
-    # print(res)
+    # node = id3(np.array(df), np.array(df.drop(columns=['好瓜'])))
+    node = c45(np.array(df), np.array(df.drop(columns=['好瓜'])))
+
+    # ['色泽', '根蒂', '敲声', '纹理', '脐部', '触感']
+    res = node.predict(['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '硬滑'])
+    print(res)
+    res = node.predict(['乌黑', '稍蜷', '沉闷', '稍糊', '稍凹', '硬滑'])
+    print(res)
